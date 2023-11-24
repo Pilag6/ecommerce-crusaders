c9c04efefd3221c30370a600cf29cdd80fb13f2d
Pilag6 <pilag6@gmail.com>
2023-11-24 09:56:43 +0100

    Update countdown display and fix star ratings

--- main.js
+++ main.js
@@ -42,8 +42,57 @@ function scrollFunction() {
     }
 }
 
-
 scrollUpButton.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
+
+// -----------------
+// COUNTDOWN
+
+// The data/time we want to countdown to
+let countDownDate = new Date("Dec 08, 2023 23:59:59").getTime();
+
+// Run myfunc every second
+
+let myfunc = setInterval(function () {
+    let now = new Date().getTime();
+    let timeleft = countDownDate - now;
+
+    // Calculating the days, hours, minutes and seconds left
+
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        if (days < 10) {
+            days = "0" + days;
+        }
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+    // Result is output to the specific element
+
+    document.querySelector("#days span").innerHTML = days;
+    document.querySelector("#hours span").innerHTML = hours;
+    document.querySelector("#mins span").innerHTML = minutes;
+    document.querySelector("#secs span").innerHTML = seconds;
+
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.querySelector("#days span").innerHTML = "00";
+        document.querySelector("#hours span").innerHTML = "00";
+        document.querySelector("#mins span").innerHTML = "00";
+        document.querySelector("#secs span").innerHTML = "00";
+        document.querySelector("#end").innerHTML = "HAPPY BIRTHDAY TO ME!";
+        document.querySelector("#end").style.display = "block";
+    }
+}, 1000);
